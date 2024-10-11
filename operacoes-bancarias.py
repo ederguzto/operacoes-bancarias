9badc517f76916a535fa0529782b6d8f24cb42e4
ederguzto <ederguzto@gmail.com>
2024-10-10 21:40:17 -0400

    Resolvendo conflitos

--- operacoes-bancarias.py
+++ operacoes-bancarias.py
@@ -1,10 +1,4 @@
-tela = (f'''
-    ------- MENU ------
-    [e] - Extrato
-    [d] - Depostiar
-    [s] - Sacar
-    [q] - Sair
-    ''')
+tela = (f'''\n    ------- MENU ------\n    [e] - Extrato\n    [d] - Depostiar\n    [s] - Sacar\n    [q] - Sair''')
 
 transacoes = []
 deposito = 0
@@ -14,22 +8,20 @@ saida = 0
 saldo = 0
 SAQUE_DIARIO = 3
 
-print(f'''{tela}
-    ''')
-opcao = input(f'''    Digite uma operação: ''').lower()
+
+
 
 while True:
+    print(f'''{tela}    \n''')
+    opcao = input(f'''    Digite uma operação: ''').lower()
     match opcao:
         case 't':
-            print(f'''
-    Você cancelou a operação anterior!
-        {tela}
-    ''')    
+            print(f'''  \nVocê cancelou a operação anterior!
+        {tela}''')    
             opcao = input(f'''    Digite uma operação: ''').lower()
 
         case 'e':
-            print(f'''
-    ----- EXTRATO -----''')
+            print(f'''\n    ----- EXTRATO -----''')
             for transacao in transacoes:
                 tipo, valor = transacao
                 if tipo == 'd':
@@ -40,60 +32,33 @@ while True:
                 else:
                     print(f'Sem transações')
             print(f'    Saldo atual: {saldo}')
-            opcao = input(f'''{tela}
-    Digite uma operação: ''').lower()
 
         case 'd':
-            deposito = input(f'''
-    ---- DEPOSITAR ----
-        
-    Digite o valor de depósito: ''')
+            deposito = input(f'''\n    ---- DEPOSITAR ----\n    Digite o valor de depósito: ''')
             if float(deposito) > 0:
                 transacoes.append (('d', (float(deposito))))
                 entrada += float(deposito)
                 saldo += float(deposito)
-                opcao = input(f'''
-    Você depositou R${deposito}{tela}
-    Digite uma operação: ''').lower()
+                print(f'''    Você depositou R${deposito}''')
             
             else:
-                deposito = input('''
-    Digite um valor válido: ''')
-            
-                opcao = input(f'''    Digite uma operação: ''').lower()
+                deposito = input('''    Digite um valor válido: ''')
 
         case 's':
-            saque = float(input(f'''
-    ------ SACAR ------
-        
-    Digite o valor de saque ou [t] para cancelar: '''))
+            saque = float(input(f'''\n    ------ SACAR ------\n    Digite o valor de saque ou [t] para cancelar: '''))
             if SAQUE_DIARIO != 0 and saldo != 0: # posso usar len() pra ver quantos tipo 's' tem na variavel transacoes e se = 3 ele nao deixa realizar o saque
-                transacoes.append (('s', float(saque)))
                 saida += saque
                 saldo -= saque
                 SAQUE_DIARIO -= 1
-                print(f'''
-    Você sacou R${saque}''')
-                print(tela)
-                opcao = input(f'''    
-    Digite uma operação: ''').lower()
+                transacoes.append (('s', float(saque)))
+                print(f'''    Você sacou R${saque}''')
             elif saque > saldo:
-                print('''
-    Saldo insuficiente''')
-                print(tela)
-                opcao = input(f'''    
-    Digite uma operação: ''').lower()
+                print('''    Saldo insuficiente''')
             else:
-                print('''
-    #### Limite de saque diário atingido! ###''')
-                print(tela)
-            
-                opcao = input(f'''    
-    Digite uma operação: ''').lower()
+                print('''    Limite de saque diário atingido!''')
 
         case 'q':
             break
 
         case _:
-            opcao = input(f'''    {tela}
-    Digite uma operação válida: ''').lower()
+            opcao = input(f'''    {tela}\n    Digite uma operação válida: ''').lower()
